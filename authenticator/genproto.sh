9f02ccee8eb320f079ea55f38d8bea94cf9012f8
vDawgg <veit.laule@gmail.com>
2022-11-19 03:24:39 +0100

    Edited proto generation for authenticator, got frontend proof of concept running

--- authenticator/genproto.sh
+++ authenticator/genproto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Proto files still need to be edited to work properly!
-python -m grpc_tools.protoc -I../proto --python_out=proto --grpc_python_out=proto ../proto/unified.proto
+# 'import unified_pb2 as unified__pb2' needs to be changed to 'from . import unified_pb2 as unified__pb2' _pb2_grpc.py after gernerating!
+python3 -m grpc_tools.protoc -I../proto --python_out=./proto --grpc_python_out=./proto ../proto/unified.proto
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	//"fmt"
+	pb "main/proto"
+	"fmt"
+	"context"
 	"html/template"
 	"log"
 	"net/http"
 	//"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Page struct {
-	Out string
-}
-
 func testHandler (w http.ResponseWriter, r *http.Request) {
-	out := "test"
-	p := &Page{Out: out}
+	type Page struct {
+		Out string
+	}
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	connAuthenticator, err := grpc.Dial("localhost:50051", opts...)
+	if err != nil {
+		fmt.Println("Unable to connect to Authenticator: ", err)
+	}
+	defer connAuthenticator.Close()
+
+	clientAuthenticator := pb.NewAuthenticatorClient(connAuthenticator)
+
+	empty := &pb.Empty{}
+	keys, err := clientAuthenticator.GetKeys(context.Background(), empty)
+	if err != nil {
+		fmt.Println("Failed to get keys: ", err)
+		return
+	}
+
+	p := &Page{Out: keys.Keys}
 	t, _ := template.ParseFiles("templates/index.html")
 	t.Execute(w, p)
 }
