f922ea4de90fcfc19344faea1f5f65bc0f521d95
vDawgg <veit.laule@gmail.com>
2023-02-16 00:30:52 +0100

    Loginpage

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"html/template"
 	"log"
-	pb "main/proto"
 	"main/helpers"
+	pb "main/proto"
 	"net/http"
 )
 
@@ -34,14 +34,36 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 
-	clientAuthenticator := helpers.GetAuthenticatorClient()
+	tmpl := template.Must(template.ParseFiles("templates/login.html"))
 
-	empty := &pb.User{}
-	token, err := clientAuthenticator.Login(context.Background(), empty)
+	clientAuthenticator := helpers.GetAuthenticatorClient()
+	
+	empty := &pb.Empty{}
+	keys, err := clientAuthenticator.GetKeys(context.Background(), empty)
 	if err != nil {
 		fmt.Println("Failed to get keys: ", err)
 		return
 	}
+	fmt.Println(keys.Keys)
+
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	//TODO: check this input for injections etc.!
+	user := &pb.User{Name: []byte(r.FormValue("name")), Password: []byte(r.FormValue("password"))}
+
+	token, err := clientAuthenticator.Login(context.Background(), user)
+	if err != nil {
+		fmt.Println("Failed to login: ", err)
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	fmt.Println(token.Token)
+
+	tmpl.Execute(w, struct{ Success bool }{true})
 
 	fmt.Println(token)
 }
